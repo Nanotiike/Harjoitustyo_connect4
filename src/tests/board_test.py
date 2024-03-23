ebb22550742957cd9443d14f8c55957eef148b74
Harri <harri.gavert@helsinki.fi>
2024-03-23 17:03:06 +0200

    tests for board.py

--- src/tests/board_test.py
+++ src/tests/board_test.py
@@ -8,3 +8,58 @@ class TestBoard(unittest.TestCase):
     
     def test_print_board(self):
         self.assertEqual(self.board.__str__(), "-----------------------------\n|   |   |   |   |   |   |   | \n-----------------------------\n|   |   |   |   |   |   |   | \n-----------------------------\n|   |   |   |   |   |   |   | \n-----------------------------\n|   |   |   |   |   |   |   | \n-----------------------------\n|   |   |   |   |   |   |   | \n-----------------------------\n|   |   |   |   |   |   |   | \n-----------------------------\n- 1 - 2 - 3 - 4 - 5 - 6 - 7 -")
+
+    def test_make_move_success(self):
+        self.assertTrue(self.board.make_move(0, "Y"))
+        self.assertEqual(self.board.board[5][0], "Y")
+    
+    def test_make_move_failure(self):
+        for i in range(6):
+            self.board.make_move(0, "Y")
+        self.assertFalse(self.board.make_move(0, "R"))
+        self.assertEqual(self.board.board[0][0], "Y")
+
+    def test_check_for_winner_horizontal(self):
+        self.board.board = [[" ", " ", " ", " ", " ", " ", " "],
+                            [" ", " ", " ", " ", " ", " ", " "],
+                            [" ", " ", " ", " ", " ", " ", " "],
+                            [" ", " ", " ", " ", " ", " ", " "],
+                            [" ", " ", " ", " ", " ", " ", " "],
+                            ["Y", "Y", "Y", "Y", " ", " ", " "]]
+        self.assertEqual(self.board.check_for_winner(), "Y")
+
+    def test_check_for_winner_vertical(self):
+        self.board.board = [[" ", " ", " ", " ", " ", " ", " "],
+                            [" ", " ", " ", " ", " ", " ", " "],
+                            ["Y", " ", " ", " ", " ", " ", " "],
+                            ["Y", " ", " ", " ", " ", " ", " "],
+                            ["Y", " ", " ", " ", " ", " ", " "],
+                            ["Y", " ", " ", " ", " ", " ", " "]]
+        self.assertEqual(self.board.check_for_winner(), "Y")
+
+    def test_check_for_winner_diagonal_1(self):
+        self.board.board = [[" ", " ", " ", " ", " ", " ", " "],
+                            [" ", " ", " ", " ", " ", " ", " "],
+                            [" ", " ", " ", "Y", " ", " ", " "],
+                            [" ", " ", "Y", " ", " ", " ", " "],
+                            [" ", "Y", " ", " ", " ", " ", " "],
+                            ["Y", " ", " ", " ", " ", " ", " "]]
+        self.assertEqual(self.board.check_for_winner(), "Y")
+
+    def test_check_for_winner_diagonal_2(self):
+        self.board.board = [["Y", " ", " ", " ", " ", " ", " "],
+                            [" ", "Y", " ", " ", " ", " ", " "],
+                            [" ", " ", "Y", " ", " ", " ", " "],
+                            [" ", " ", " ", "Y", " ", " ", " "],
+                            [" ", " ", " ", " ", " ", " ", " "],
+                            [" ", " ", " ", " ", " ", " ", " "]]
+        self.assertEqual(self.board.check_for_winner(), "Y")
+    
+    def test_check_for_winner_no_winner(self):
+        self.board.board = [[" ", " ", " ", " ", " ", " ", " "],
+                            [" ", "R", " ", " ", " ", " ", " "],
+                            [" ", "Y", " ", " ", " ", " ", " "],
+                            [" ", "Y", " ", " ", " ", " ", " "],
+                            [" ", "R", " ", " ", " ", " ", " "],
+                            ["Y", "Y", "R", "Y", " ", " ", " "]]
+        self.assertEqual(self.board.check_for_winner(), None)
